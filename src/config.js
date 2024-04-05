5011d6c2e249fb2926ee671783d8c1c83b57d877
RIYAARORA20 <riyaarora202003@gmail.com>
2024-04-05 20:33:26 +0530

    Removed / from BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://medicarebackend-1-f999.onrender.com/";
+export const BASE_URL = "https://medicarebackend-1-f999.onrender.com";
 export const token = localStorage.getItem('token');
